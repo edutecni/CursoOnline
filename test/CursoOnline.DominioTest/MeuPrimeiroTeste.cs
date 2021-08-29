d5661a645041f51d1bd2a85558774a16f5bd6b2d
Eduardo Alves Canuto <edutecni@gmail.com>
2021-08-29 08:03:23 -0300

    Ok

---
+++ test/CursoOnline.DominioTest/MeuPrimeiroTeste.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest
+{
+    public class MeuPrimeiroTeste
+    {
+        [Fact(DisplayName = "Testar2")]
+        public void DeveAsVariaveisTeremOMesmoValor()
+        {
+            // Organização
+            var variavel1 = 1;
+            var variavel2 = 1;
+
+            // Ação
+
+            // Assert
+            Assert.True(variavel1 == variavel2);
+        }
+    }
+}

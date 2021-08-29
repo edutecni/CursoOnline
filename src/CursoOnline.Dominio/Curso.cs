1d999b6861bc91124dfa635e2001c0b642bcb244
Eduardo Alves Canuto <edutecni@gmail.com>
2021-08-29 09:48:27 -0300

    Ok2

---
+++ src/CursoOnline.Dominio/Curso.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoOnline.Dominio.Cursos
+{
+    public class Curso
+    {
+        
+        public string Nome { get; set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double ValorCurso { get; private set; }
+
+        //public Curso() { }
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valorCurso)
+        {
+            this.Nome = nome;
+            this.CargaHoraria = cargaHoraria;
+            this.PublicoAlvo = publicoAlvo;
+            this.ValorCurso = valorCurso;
+        }
+    }
+}
---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CursoOnline.Dominio.Cursos;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        // "Eu, enquanto administrador quero criar e editar curso parq que sejam abertas instrções para o mesmo."
+
+        // Critério de aceite
+
+        // - Criar um curso com, nome carga hotária, publico alvo e valor do curso.
+        // - As opções para público alvo são: Estudante, Universitário, Empregado e Empregador.
+        // - Todos os campos do curso são obrigatório.
+
+
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            // Arrange (Organização)
+            const string nome = "Informática Básica";
+            const double cargaHoraria = 80;
+            const string publicoAlvo = "Estudantes";
+            const double valorCurso = 950;
+
+            // Act (Ação)
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valorCurso);
+
+            // Assert
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valorCurso, curso.ValorCurso);
+        }
+    }
+}

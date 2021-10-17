ab9f6124105a7e597e6c8c847c343c5cb010e719
Eduardo Alves Canuto <edutecni@gmail.com>
2021-10-17 18:10:42 -0300

    2. Mocks

---
+++ test/CursoOnline.DominioTest/Cursos/ArmazenadorDeCurso.cs
@@ -0,0 +1,23 @@
+﻿using CursoOnline.Dominio.Cursos;
+using System;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    internal class ArmazenadorDeCurso
+    {
+        private ICursoRepository _cursoRepository;
+
+        public ArmazenadorDeCurso(ICursoRepository cursoRepository)
+        {
+            _cursoRepository = cursoRepository;
+        }
+
+        internal void Armazenar(CursoDto cursoDto)
+        {
+            var curso = new Curso(cursoDto.Nome, cursoDto.Descricao, cursoDto.CargaHoraria, PublicoAlvo.Estudante, cursoDto.Valor);
+
+            _cursoRepository.Adicionar(curso);
+
+        }
+    }
+}
---
+++ test/CursoOnline.DominioTest/Cursos/ArmazenadorDeCursoTeste.cs
@@ -0,0 +1,46 @@
+﻿using CursoOnline.Dominio.Cursos;
+using Moq;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class ArmazenadorDeCursoTeste
+    {
+        [Fact]
+        public void DeveAdicionarCurso()
+        {
+            var cursoDto = new CursoDto
+            {
+                Nome = "Curso A",
+                Descricao = "Descricao",
+                CargaHoraria = 80,
+                PublicoAlvoId = 1,
+                Valor = 850.00
+            };
+
+            var cursoRepositoryMock = new Mock<ICursoRepository>();
+
+            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositoryMock.Object);
+
+            armazenadorDeCurso.Armazenar(cursoDto);
+
+            cursoRepositoryMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+
+        }
+    }
+
+    public interface ICursoRepository
+    {
+        void Adicionar(Curso curso);
+        void Atualizar(Curso curso);
+    }
+
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; set; }
+        public int PublicoAlvoId { get; set; }
+        public double Valor { get; set; }
+    }
+}
